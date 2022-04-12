553b52d1e80618d6d098f49110e35bb484595d85
Mykyta Kamak <mykytakamak@gmail.com>
2022-04-12 15:10:19 +0300

    Add windows copy-paste

--- lua/ratibram/options.lua
+++ lua/ratibram/options.lua
@@ -37,6 +37,19 @@ local options = {
 
 vim.opt.shortmess:append "c"
 
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf"
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf"
+  },
+  cache_enable = 0,
+}
+
 for k, v in pairs(options) do
     vim.opt[k] = v
 end
